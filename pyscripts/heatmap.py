0b0bb7d6e312bb67282203e0d03adc7d747e778f
jacqpat <patrickjacques@laposte.net>
2024-04-26 15:47:42 +0200

    heatmap import

--- pyscripts/heatmap.py
+++ pyscripts/heatmap.py
@@ -7,6 +7,11 @@ parser = op.OptionParser(conflict_handler="resolve")
 parser.add_option( '-i', '--input', dest='input', default='', action='store', help='The input file is a csv table with all the data' )
 parser.add_option('-s', '--sep', dest='separator', default=',', action='store', help='By default, the output separator is a standard ","')
 parser.add_option('-h', '--header', dest='header', default=0, action='store', help='Does the input file have a header or not? Must be int, list of int, or None.')
+parser.add_option('-x', '--idx', dest='index', default='', action='store', help='Column to use as row index for the data (ex: samples names)')
+parser.add_option('-d', '--colstart', dest='colstart', default=0, action='store', help='Starting column of the contamination data.')
 ( options, spillover ) = parser.parse_args()
 # Import data
-df1 = pd.read_csv(options.input, sep = options.separator, header = int(options.header)).dropna().reset_index(drop=True)
+df1 = pd.read_csv(options.input, sep = options.separator, header = int(options.header))
+print(df1)
+df_data = df1.iloc[:, int(options.colstart):]
+print(df_data)
