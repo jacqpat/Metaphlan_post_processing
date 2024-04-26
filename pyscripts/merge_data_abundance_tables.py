83eb99d429bef6332b31de46859c2d75bc74446c
jacqpat <patrickjacques@laposte.net>
2024-04-26 10:28:17 +0200

    more options

--- pyscripts/merge_data_abundance_tables.py
+++ pyscripts/merge_data_abundance_tables.py
@@ -29,6 +29,8 @@ def main():
     parser.add_option( '-d', '--input2', dest='input_data', default='', action='store', help='The input file is a csv table with general samples data' )
     parser.add_option( '-o', '--output', dest='output', default='', action='store', help='the output file is the merged csv table' )
     parser.add_option('-h', '--header', dest='header', default=0, action='store', help='Does the input file have a header or not? Must be int, list of int, or None.')
+    parser.add_option('-c', '--samples1', dest='samples_conta', default='', action='store', help='The name of the column with the samples names for the first input file')
+    parser.add_option('-k', '--samples2', dest='samples_data', default='', action='store', help='The name of the column with the samples names for the second input file')
     parser.add_option('-s', '--sep', dest='separator', default=',', action='store', help='By default, the output separator is a standard ","')
     ( options, spillover ) = parser.parse_args()
     # Error handling
@@ -39,20 +41,20 @@ def main():
     df1 = pd.read_csv(options.input_conta, sep = options.separator, header = int(options.header)).dropna().reset_index(drop=True)
     print("Left Dataframe: Contamination")
     print(df1)
-    df1['Sample'] = df1['Sample'].str.replace('_','-')
-    df1['Sample'] = df1['Sample'].str.replace('-S\d+$','',regex=True)
+    df1[options.samples_conta] = df1[options.samples_conta].str.replace('_','-')
+    df1[options.samples_conta] = df1[options.samples_conta].str.replace('-S\d+$','',regex=True)
     df2 = pd.read_csv(options.input_data, sep = options.separator)
     print("Right Dataframe: Dataset")
     print(df2)
-    df2['Sample'] = df2['Sample'].str.replace('_','-')
-    if set(df1['Sample']) == set(df2['Sample']):
+    df2[options.samples_data] = df2[options.samples_data].str.replace('_','-')
+    if set(df1[options.samples_conta]) == set(df2[options.samples_data]):
         # merge the two dataframes
         df3 = pd.merge(df2, df1, how = 'inner', on = 'Sample')
         df3.to_csv(options.output, header = True, index = False, sep = options.separator)
     else:
         print("ERROR: the two dataframes 'samples' columns are different!")
-        print(df1['Sample'].to_list())
-        print(df2['Sample'].to_list())
+        print(df1[options.samples_conta].to_list())
+        print(df2[options.samples_data].to_list())
 
 if __name__ == '__main__':
     main()
